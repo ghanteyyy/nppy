6babfdcc4a1c80817900f4d3f53dbc490a3b8a97
6292santosh <6292santosh@gmail.com>
2019-06-07 21:06:24 +0545

    Improved Startup

--- PROJECT GUIs/DIGITAL CLOCK/Digital Clock.py
+++ PROJECT GUIs/DIGITAL CLOCK/Digital Clock.py
@@ -2,34 +2,15 @@ import time
 
 try:  # Python 3
     from tkinter import Tk, Frame, Label
-    from winreg import ConnectRegistry, OpenKey, SetValueEx, KEY_WRITE, KEY_SET_VALUE, REG_SZ, HKEY_CURRENT_USER    # Python 3.x
 
 except (ImportError, ModuleNotFoundError):  # Python 2
     from Tkinter import Tk, Frame, Label
-    from _winreg import ConnectRegistry, OpenKey, SetValueEx, KEY_WRITE, KEY_SET_VALUE, REG_SZ, HKEY_CURRENT_USER    # Python 2.x
-
-
-def startup():
-    '''Add to the startup'''
-
-    areg = ConnectRegistry(None, HKEY_CURRENT_USER)
-
-    try:
-        akey = OpenKey(areg, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run\Digital Clock.exe', 0, KEY_WRITE)
-        areg.Close()
-        akey.Close()
-
-    except WindowsError:
-        key = OpenKey(areg, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run', 0, KEY_SET_VALUE)
-        SetValueEx(key, 'Digital Clock', 0, REG_SZ, r'C:\ProgramData\Digital Clock\Digital Clock.exe')
-        areg.Close()
-        key.Close()
 
 
 def get_time():
     '''Get Current Date and Time'''
 
-    label.config(text=time.strftime('%B %d %a %Y\n%I:%M:%S %p'))
+    label.config(text=time.strftime('%b %d %a %Y\n%I:%M:%S %p'))
     root.after(200, get_time)
 
 
@@ -39,6 +20,8 @@ def Clock():
     global root, label
 
     root = Tk()
+    root.withdraw()
+    root.after(0, root.deiconify)
     root.config(bg='red')
     root.overrideredirect(True)
     root.wm_attributes("-topmost", 1, "-transparentcolor", 'red')
@@ -54,5 +37,4 @@ def Clock():
 
 
 if __name__ == '__main__':
-    startup()
     Clock()
