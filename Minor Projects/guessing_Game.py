f74fbbdd094a91f8acd7a50f191ec432f79181d5
ghanteyyy <ghanteyyy@gmail.com>
2020-08-21 20:51:44 +0545

    Fixed Stack Overflow Error

--- Minor Projects/guessing_Game.py
+++ Minor Projects/guessing_Game.py
@@ -9,35 +9,32 @@ class Guessing_Game:
     def is_guess_within_range(self, guess):
         '''Check if the guess is within the range of self.low and self.high'''
 
-        if self.low <= guess <= self.high:
-            return True
-
-        return False
+        return self.low <= guess <= self.high
 
     def get_user_guess(self):
         '''Ask user to enter their guess until the valid guess is entered.'''
 
-        try:
-            guess = int(input(f'\nGuess a number between {self.low}-{self.high}: '))
+        guess = False
 
-            if not self.is_guess_within_range(guess):
-                print(f'Your guess is not between {self.low}-{self.high}')
+        while not guess:
+            try:
+                guess = int(input(f'\nGuess a number between {self.low}-{self.high}: '))
 
-            else:
-                return guess
+                if not self.is_guess_within_range(guess):
+                    print(f'Your guess must be in {self.low}-{self.high}')
+                    guess = False
 
-        except ValueError:
-            print('Your guess is not valid. Guess was expected in integer')
+            except ValueError:
+                print('Your guess is not valid. Guess was expected in integer')
+
+        return guess
 
     def play_again(self):
         '''Ask user if they want to play the game again.'''
 
         check = input('\nDo you want to play again (y/n)?').lower()
 
-        if check == 'y':
-            return True
-
-        return False
+        return check == 'y'
 
     def random_number(self):
         '''Returns random number which has to be guessed by the user'''
@@ -47,33 +44,30 @@ class Guessing_Game:
     def main(self):
         '''Playing the game'''
 
-        count = 0
-        is_guessed = False
-        is_valid_guess = False
-        to_guess = self.random_number()
+        play = True
 
-        while not is_guessed:   # Until user guesses the random_number
-            while not is_valid_guess:  # Until users enters valid guess
+        while play:
+            count = 1
+            is_guessed = False
+            to_guess = self.random_number()
+
+            while not is_guessed:   # Until user guesses the random_number
                 user_guess = self.get_user_guess()
 
-                if user_guess:
-                    is_valid_guess = True
+                if user_guess == to_guess:
+                    is_guessed = True
+                    print(f'You have guessed in {count} guesses')
 
-            if user_guess == to_guess:
-                is_guessed = True
-                print(f'You have guessed in {count} guesses')
+                    if not self.play_again():
+                        play = False
 
-                if self.play_again():
-                    self.main()
+                elif user_guess > to_guess:
+                    print('Your guess is high')
 
-            elif user_guess > to_guess:
-                print('Your guess is high')
+                elif user_guess < to_guess:
+                    print('Your guess is low')
 
-            elif user_guess < to_guess:
-                print('Your guess is low')
-
-            count += 1
-            is_valid_guess = False
+                count += 1
 
 
 if __name__ == '__main__':
