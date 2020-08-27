6d21d0fa35ddbd7c9caffb4ecffc8e1d73989982
ghanteyyy <ghanteyyy@gmail.com>
2020-08-27 20:05:47 +0545

    Fixed broken functionality

--- PROJECT GUIs/REMAINDER/main_window.py
+++ PROJECT GUIs/REMAINDER/main_window.py
@@ -1,17 +1,13 @@
 import os
 import sys
 from tkinter import *
+import tkinter.ttk as ttk
 from tkinter import messagebox
-from tkinter.ttk import Combobox
 
 
 class GUI:
     def __init__(self):
         self.master = Tk()
-        self.master.resizable(0, 0)
-        self.master.title('REMIND ME !')
-        self.master.config(bg='#6200ff')
-        self.master.iconbitmap(self.resource_path('included_files/icon.ico'))
 
         self.width, self.height = 446, 200
         self.screen_width, self.screen_heigth = self.master.winfo_screenwidth() // 2, self.master.winfo_screenheight() // 2
@@ -21,40 +17,60 @@ class GUI:
         self.title_label.pack()
 
         self.entry_box_var = StringVar()
-        self.entry_box = Entry(self.master, fg='#8a86ad', width=33, font=('ISOCP', 12, 'bold'), textvariable=self.entry_box_var, justify=CENTER)
+        self.entry_box_style = ttk.Style()
+        self.entry_box_style.configure('E.TEntry', foreground='grey')
+        self.entry_box = ttk.Entry(self.master, width=35, justify=CENTER, font=('Helvetica', 12, 'bold'), textvariable=self.entry_box_var, style='E.TEntry')
         self.entry_box_var.set('Remind Me About ...')
-        self.entry_box.pack(pady=10)
+        self.entry_box.pack(pady=10, ipady=2)
 
-        self.combo_box_frame = Frame(self.master)
+        self.combo_box_frame = Frame(self.master, bg='#6200ff')
 
         self.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        self.combo_box_month = Combobox(self.combo_box_frame, value=self.months, width=8)
+        self.combo_box_month = ttk.Combobox(self.combo_box_frame, value=self.months, width=8)
         self.combo_box_month.set('Month')
-        self.combo_box_month.pack(side=LEFT)
+        self.combo_box_month.pack(side=LEFT, padx=1, ipady=2)
 
-        self.combo_box_date = Combobox(self.combo_box_frame, value=self.get_combo_box_values(1, 32), width=5)
+        self.combo_box_date = ttk.Combobox(self.combo_box_frame, value=self.get_combo_box_values(1, 32), width=5)
         self.combo_box_date.set('Date')
-        self.combo_box_date.pack(side=LEFT)
+        self.combo_box_date.pack(side=LEFT, padx=1, ipady=2)
 
-        self.combo_box_hour = Combobox(self.combo_box_frame, value=self.get_combo_box_values(1, 12), width=5)
+        self.combo_box_hour = ttk.Combobox(self.combo_box_frame, value=self.get_combo_box_values(1, 12), width=5)
         self.combo_box_hour.set('Hour')
-        self.combo_box_hour.pack(side=LEFT)
+        self.combo_box_hour.pack(side=LEFT, padx=1, ipady=2)
 
-        self.combo_box_minute = Combobox(self.combo_box_frame, value=self.get_combo_box_values(0, 59), width=7)
+        self.combo_box_minute = ttk.Combobox(self.combo_box_frame, value=self.get_combo_box_values(0, 59), width=7)
         self.combo_box_minute.set('Minute')
-        self.combo_box_minute.pack(side=LEFT)
+        self.combo_box_minute.pack(side=LEFT, padx=1, ipady=2)
 
-        self.combo_box_am_pm = Combobox(self.combo_box_frame, value=['AM', 'PM'], width=8)
+        self.combo_box_am_pm = ttk.Combobox(self.combo_box_frame, value=['AM', 'PM'], width=8)
         self.combo_box_am_pm.set('AM / PM')
-        self.combo_box_am_pm.pack(side=LEFT)
+        self.combo_box_am_pm.pack(side=LEFT, padx=1, ipady=2)
 
-        self.combo_box_frame.pack(pady=10)
+        self.combo_box_frame.pack(pady=3)
 
         self.add_button = Button(self.master, text='ADD REMAINDER', width=15, fg='#cfb0be', bg='#6200ff', font=('ISOCP', 15, 'bold'), activebackground='#6200ff', activeforeground='#cfb0be', border=0, command=self.add_command, cursor='hand2')
         self.add_button.pack(pady=10)
 
+        self.master.after(0, self.initial_position)
         self.master.bind('<Button-1>', self.key_bindings)
         self.master.mainloop()
+
+    def initial_position(self):
+        '''Start the program in the center of the screen'''
+
+        self.master.withdraw()
+        self.master.update()
+
+        width, height = self.master.winfo_width(), self.master.winfo_height() - 5
+        screen_width, screen_heigth = self.master.winfo_screenwidth() // 2, self.master.winfo_screenheight() // 2
+        self.master.geometry(f'{width}x{height}+{screen_width - width // 2}+{screen_heigth - height // 2}')
+
+        self.master.resizable(0, 0)
+        self.master.title('REMIND ME !')
+        self.master.config(bg='#6200ff')
+        self.master.iconbitmap(self.resource_path('included_files/icon.ico'))
+
+        self.master.deiconify()
 
     def key_bindings(self, event):
         '''Action when user clicks inside entry widget or oustide of the entry widget'''
@@ -64,12 +80,12 @@ class GUI:
         if event.widget == self.entry_box:
             if get_from_entry == 'Remind Me About ...':
                 self.entry_box_var.set('')
-                self.entry_box.config(fg='black')
+                self.entry_box_style.configure('E.TEntry', foreground='black')
 
-        elif event.widget in [self.master, self.title_label]:
+        else:
             if not get_from_entry:
                 self.entry_box_var.set('Remind Me About ...')
-                self.entry_box.config(fg='grey')
+                self.entry_box_style.configure('E.TEntry', foreground='grey')
 
             self.master.focus()
 
@@ -95,7 +111,7 @@ class GUI:
             elif month == 'Month' or month not in self.months:
                 messagebox.showerror('Invalid Month', 'Select Valid Month')
 
-            elif not date or date == 'Date' or not int(date) in range(1, 13):
+            elif not date or date == 'Date' or not int(date) not in range(1, 13):
                 messagebox.showerror('Invalid Date', 'Date must be in between 1-12')
 
             elif not hour or hour == 'Hour' or int(hour) not in range(1, 13):
@@ -117,12 +133,13 @@ class GUI:
                     f.write(write)
 
                 self.entry_box_var.set('Remind Me About ...')
-                self.entry_box.config(fg='grey')
+                self.entry_box_style.configure('E.TEntry', foreground='grey')
 
                 self.combo_box_month.set('Month')
                 self.combo_box_hour.set('Hour')
+                self.combo_box_date.set('Date')
                 self.combo_box_minute.set('Minute')
-                self.combo_box_am_pm.set('AM/PM')
+                self.combo_box_am_pm.set('AM / PM')
                 self.master.focus()
 
                 messagebox.showinfo('Remainder Added', f'You will reminded in {month} {date} {hour}:{minute} {am_pm}')
