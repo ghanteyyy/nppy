e359d6755ffc214272a3930bb00a2b875f4bdcee
ghanteyyy <ghanteyyy@gmail.com>
2020-10-31 13:36:24 +0545

    Used f-string instead of format

--- Minor Projects/os_details.py
+++ Minor Projects/os_details.py
@@ -5,23 +5,18 @@ import platform
 def os_details():
     '''Returns some information regrading your Operating System (OS)'''
 
-    print('''
-    COMPUTER NAME           : {}
-    USERNAME                : {}
-    NUMBER OF PROCESSORS    : {}
-    PROCESSOR IDENTIFIER    : {}
-    OS                      : {}
-    HOME DRIVE              : {}
-    PROCESSOR LEVEL         : {}
-    SYSTEMDRIVE             : {}
-    SYSTEMROOT              : {}
-    PROCESSOR ARCHITECTURE  : {}
-    SYSTEM TYPE             : {}
-    WINDOWS EDITION         : {}
-        '''.format(os.environ['COMPUTERNAME'], os.environ['USERNAME'], os.environ['NUMBER_OF_PROCESSORS'],
-                   os.environ['PROCESSOR_IDENTIFIER'], os.environ['OS'], os.environ['HOMEDRIVE'],
-                   os.environ['PROCESSOR_LEVEL'], os.environ['SYSTEMDRIVE'], os.environ['SYSTEMROOT'],
-                   os.environ['PROCESSOR_ARCHITECTURE'], platform.architecture()[0], platform.win32_ver()[0]))
+    print(f'''
+    COMPUTER NAME                      : {os.environ['COMPUTERNAME']}
+    USERNAME                           : {os.environ['USERNAME']}
+    NUMBER OF PROCESSORS               : {os.environ['NUMBER_OF_PROCESSORS']}
+    PROCESSOR IDENTIFIER               : {os.environ['PROCESSOR_IDENTIFIER']}
+    OS                                 : {os.environ['OS']}
+    HOME DRIVE/SYSTEMDRIVE             : {os.environ['HOMEDRIVE']}
+    PROCESSOR LEVEL                    : {os.environ['PROCESSOR_LEVEL']}
+    SYSTEMROOT                         : {os.environ['SYSTEMROOT']}
+    PROCESSOR ARCHITECTURE             : {os.environ['PROCESSOR_ARCHITECTURE']}
+    SYSTEM TYPE                        : {platform.architecture()[0]}
+    WINDOWS EDITION                    : {platform.win32_ver()[0]}''')
 
 
 if __name__ == '__main__':
