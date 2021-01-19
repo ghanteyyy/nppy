12fdfa69e6b0229a594a90a946e1912d5d2c58e4
ghanteyyy <ghanteyyy@gmail.com>
2021-01-19 07:09:21 +0545

    Updated build number

--- PROJECT GUIs/ZPAD/about.py
+++ PROJECT GUIs/ZPAD/about.py
@@ -6,7 +6,7 @@ import search
 
 class About:
     def __init__(self, master):
-        self.build_number = '1.0.8'
+        self.build_number = '1.0.9'
 
         self.master = master
         self.author_page_link = 'http://github.com/ghanteyyy'
