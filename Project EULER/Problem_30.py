c8c2857b0ccf81d7df04a82093b4714351c618c3
ghanteyyy <ghanteyyy@gmail.com>
2021-03-23 00:54:30 +0545

    Added New Project

---
+++ Project EULER/Problem_30.py
@@ -0,0 +1,23 @@
+'''
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+1634 = 14 + 64 + 34 + 44
+8208 = 84 + 24 + 04 + 84
+9474 = 94 + 44 + 74 + 44
+As 1 = 14 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+'''
+
+sums = 0
+
+for i in range(10, 5 * 9 ** 5):
+    value = sum([int(i) ** 5 for i in str(i)])
+
+    if value == i:
+        sums += i
+
+print(sums)
---
+++ Project EULER/Problem_36.py
@@ -0,0 +1,33 @@
+'''
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+'''
+
+
+def into_binary(nums):
+    binary = ''
+
+    while nums:
+        rem = nums % 2
+        nums = nums // 2
+        binary += str(rem)
+
+    return is_palindrome(binary)
+
+
+def is_palindrome(num):
+    num = str(num)
+    return num == num[::-1]
+
+
+sums = 0
+
+
+for i in range(1, 1000000):
+    if is_palindrome(i) and into_binary(i):
+        sums += i
+
+print(sums)
