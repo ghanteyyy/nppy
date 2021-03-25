78877a1b9e57c84ca7d582fcc24e19ea32e35402
ghanteyyy <ghanteyyy@gmail.com>
2021-03-25 08:13:52 +0545

    Made window size dynamic when centering to the screen Updated UI Optimized overall performance

--- PROJECT GUIs/BLINK TEXT/blink_text.py
+++ PROJECT GUIs/BLINK TEXT/blink_text.py
@@ -9,42 +9,52 @@ class Blink:
     def __init__(self):
         self.master = Tk()
         self.master.withdraw()
-        self.master.resizable(0, 0)
-        self.master.after(0, self.master.deiconify)
         self.master.iconbitmap(self.resource_path('included_files/icon.ico'))
         self.master.title('Blinking Text')
 
-        self.width, self.height = 302, 99
-        self.master.geometry(f'{self.width}x{self.height}+{self.master.winfo_screenwidth() // 2 - self.width // 2}+{self.master.winfo_screenheight() // 2 - self.height // 2}')
-
         self.frame = Frame(self.master, bg='#422a91')
-        self.frame.pack()
+        self.frame.pack(padx=16)
 
         self.entry_var = StringVar()
-        self.entry = Entry(self.frame, width=15, font=('Courier', 15, 'bold'), fg='grey', justify='center', textvariable=self.entry_var)
         self.entry_var.set('TEXT')
-        self.entry.pack(pady=10, ipady=2, side=LEFT)
+        self.entry = Entry(self.frame, width=15, font=('Courier', 15, 'bold'), fg='grey', justify='center', textvariable=self.entry_var)
+        self.entry.grid(row=0, column=0, pady=10, ipady=2)
 
-        self.add_button = Button(self.frame, text='ADD', fg='white', bg='#422a91', activebackground='#422a91', activeforeground='white', relief=GROOVE, cursor='hand2', command=self.add_button_command)
-        self.add_button.pack(side=LEFT, padx=10, ipadx=15, ipady=3)
+        self.add_button = Button(self.frame, text='ADD', fg='white', bg='green', activebackground='green', activeforeground='white', relief=GROOVE, cursor='hand2', command=self.change_text)
+        self.add_button.grid(row=0, column=1, padx=10, ipadx=15, ipady=3)
 
-        self.text = Label(self.master, text='Blink', fg='white', bg='#422a91', font=('Courier', 20))
-        self.text.pack(pady=2)
+        self.text_var = StringVar()
+        self.text_var.set('Blink')
+        self.text = Label(self.master, text='Blink', fg='white', bg='#422a91', font=('Courier', 20), textvariable=self.text_var)
+        self.text.pack(pady=1, ipady=5)
 
         self.entry.bind('<FocusIn>', self.bind_keys)
+        self.entry.bind('<Return>', self.change_text)
         self.master.bind('<Button-1>', self.bind_keys)
         self.add_button.bind('<FocusIn>', self.bind_keys)
-        self.entry.bind('<Return>', self.add_button_command)
-        self.add_button.bind('<Return>', self.add_button_command)
+        self.add_button.bind('<Return>', self.change_text)
 
-        self.master.after(0, self.blink)
+        self.initial_position()
         self.master.config(bg='#422a91')
         self.master.mainloop()
 
-    def bind_keys(self, event):
-        '''Commands when user clicks in and out of the entry widget'''
+    def initial_position(self):
+        '''Centering window when program starts'''
 
-        entry_get = self.entry.get().strip()
+        self.master.update()
+
+        width, height = self.master.winfo_width() // 2, self.master.winfo_height() // 2
+        screen_width, screen_height = self.master.winfo_screenwidth() // 2, self.master.winfo_screenheight() // 2
+
+        self.master.geometry(f'+{screen_width - width}+{screen_height - height}')
+        self.master.resizable(0, 0)
+        self.master.deiconify()
+        self.blink()
+
+    def bind_keys(self, event):
+        '''Change focus when user clicks in and out of the entry widget'''
+
+        entry_get = self.entry_var.get().strip()
 
         if event.widget == self.entry:
             if entry_get == 'TEXT':
@@ -70,19 +80,18 @@ class Blink:
 
         self.master.after(100, self.blink)
 
-    def add_button_command(self, event=None):
+    def change_text(self, event=None):
         '''Command for "ADD" button'''
 
         get_from_entry = self.entry.get().strip()
 
         if get_from_entry:
-            self.text.config(text=get_from_entry)
+            self.text_var.set(get_from_entry)
 
         else:
-            self.text.config(text='Blink')
+            self.text_var.set('Blink')
 
         self.master.focus()
-        self.entry.delete(0, END)
         self.entry_var.set('TEXT')
         self.entry.config(fg='grey')
 
