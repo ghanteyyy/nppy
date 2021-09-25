3ccc387cf9b826026d3dd0862adfc15c4e42067a
ghanteyyy <ghanteyyy@gmail.com>
2021-09-25 17:41:02 +0545

    Solved new problem

---
+++ Project EULER/Problem_39.py
@@ -0,0 +1,33 @@
+'''
+If p is the perimeter of a right angle triangle with integral length sides, {a,b,c},
+there are exactly three solutions for p = 120.
+
+{20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p ≤ 1000, is the number of solutions maximised?
+'''
+
+counter = {}
+total_perimeter = 1000
+highest_count, result = 0, 0
+
+for p in range(1, total_perimeter // 3):
+    for b in range(p, 500):
+        h = (p ** 2 + b ** 2) ** 0.5
+        sums = int(p + b + h)
+
+        if int(h) == h and sums <= total_perimeter:
+            # Counting the number of repetitions of sum of p, b and h
+            if sums in counter:
+                counter[sums] += 1
+
+            else:
+                counter[sums] = 1
+
+# Getting value whose number of repetitions is the highest
+for val, count in counter.items():
+    if count > highest_count:
+        result = val
+        highest_count = count
+
+print(result)
