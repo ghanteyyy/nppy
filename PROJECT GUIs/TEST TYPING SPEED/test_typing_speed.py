ede4b3c2459ab7b9717c2cad8d6c2ada15c86a75
ghanteyyy <ghanteyyy@gmail.com>
2021-10-18 10:39:37 +0545

    Removed redundant codes Improved grammatical errors Improved adding random text Improved calculating accuracy Renamed some variables for readability Improved counting number of backspace key pressed Restricting getting new word selection if nothing is typed in typing area

--- PROJECT GUIs/TEST TYPING SPEED/test_typing_speed.py
+++ PROJECT GUIs/TEST TYPING SPEED/test_typing_speed.py
@@ -9,16 +9,6 @@ from tkinter import PhotoImage, font
 
 class Test_Typing_Speed:
     def __init__(self):
-        self.time = 60
-        self.running = False
-        self.correct_words = 0
-        self.total_keywords = 0
-        self.incorrect_words = 0
-        self.backspace_counter = 0
-        self.correct_keystrokes = 0
-        self.incorrect_keystrokes = 0
-
-        self.prev_index = '1.0'
         self.redo_path = self.resource_path('redo.png')
         self.icon_path = self.resource_path('icon.ico')
         self.file_path = self.resource_path('words.txt')
@@ -38,7 +28,6 @@ class Test_Typing_Speed:
         self.typing_area.focus_set()
 
         self.time_label_var = StringVar()
-        self.time_label_var.set('01:00')
         self.time_label = Label(self.typing_area_frame, textvariable=self.time_label_var, font=font.Font(size=15), relief=SOLID)
         self.time_label.pack(side=LEFT, ipadx=1, ipady=4, padx=10)
 
@@ -47,6 +36,7 @@ class Test_Typing_Speed:
         self.redo_button.pack(side=LEFT)
         self.typing_area_frame.pack(pady=5)
 
+        self.default_values()
         self.insert_to_text_widget()
         self.initial_position()
 
@@ -59,9 +49,25 @@ class Test_Typing_Speed:
         self.show_words.bind('<Motion>', self.forbid_default_bindings)
         self.show_words.bind('<MouseWheel>', self.forbid_default_bindings)
         self.typing_area.bind('<KeyRelease>', self.forbid_default_bindings)
+        self.typing_area.bind('<Control-BackSpace>', self.control_backspace)
         self.show_words.bind('<Double-Button-1>', self.forbid_default_bindings)
 
         self.master.mainloop()
+
+    def default_values(self):
+        self.TotalTime = 60
+        self.running = False
+        self.CorrectWords = 0
+        self.prev_index = '1.0'
+        self.TotalKeyWords = 0
+        self.IncorrectWords = 0
+        self.TwoCharacterWord = 0
+        self.BackSpaceCounter = 0
+        self.CorrectKeyStrokes = 0
+        self.typing_area_var.set('')
+        self.IncorrectKeyStrokes = 0
+        self.SingleCharacterWord = 0
+        self.time_label_var.set('01:00')
 
     def initial_position(self):
         '''Set position of the window when opened for the first time'''
@@ -103,14 +109,25 @@ class Test_Typing_Speed:
 
             return 'break'
 
+    def control_backspace(self, event=None):
+        '''When control backspace key is pressed'''
+
+        var_get = self.typing_area_var.get().strip()
+
+        if var_get:
+            self.typing_area_var.set('')
+            self.BackSpaceCounter += len(var_get)
+
+        return 'break'
+
     def backspace(self, event=None):
         '''When backspace key is pressed'''
 
-        self.backspace_counter += 1
         var_get = self.typing_area_var.get()
-        selected_value = self.show_words.get('select.first', 'select.last')
 
         if var_get:
+            self.BackSpaceCounter += 1
+            selected_value = self.show_words.get('select.first', 'select.last')
             self.typing_area_var.set(var_get[:-1])
 
             if selected_value.startswith(var_get[:-1]):
@@ -132,23 +149,36 @@ class Test_Typing_Speed:
         '''Insert random word from text-file when program opened for the
            first time or when redo button is clicked or F5 button is pressed'''
 
-        random_words = ''
+        count = 0
+        prev_text = ''
         self.show_words.config(state='normal')
         self.show_words.delete('1.0', 'end')
 
-        for _ in range(220):
-            text = random.choice(self.contents)
+        while count <= 300:
+            success = False
+            text = random.choice(self.contents).strip()
+            text_length = len(text)
 
-            if text == 'i':
-                random_words += f'{text.upper()} '
+            if text_length in range(1, 9) and text != prev_text and text.isalpha():
+                if text_length == 1:
+                    if self.SingleCharacterWord != 3:
+                        success = True
+                        text = text.title()
+                        self.SingleCharacterWord += 1
 
-            elif random.randint(0, 100) in range(20, 30):  # Make the first letter of the word capital when the random value is between 20-30. This is my preferences.
-                random_words += f'{text.title()} '
+                elif text_length == 2:
+                    if self.TwoCharacterWord != 3:
+                        success = True
+                        self.TwoCharacterWord += 1
 
-            else:
-                random_words += f'{text} '
+                else:
+                    success = True
 
-        self.show_words.insert('1.0', random_words)  # Inserting the random value to the text-widget
+                if success:
+                    count += 1
+                    prev_text = text
+                    self.show_words.insert('1.0', text + ' ')  # Inserting the random value to the text-widget
+
         self.show_words.config(state='disabled')
         self.get_space_index()
         self.master.after(10, self.select_word)
@@ -192,27 +222,29 @@ class Test_Typing_Speed:
             self.track_time()
 
         get_value = self.typing_area_var.get().strip()
-        selected_value = self.show_words.get('select.first', 'select.last')
-        len_selected_value = len(selected_value)
 
-        self.total_keywords += len_selected_value
+        if get_value:
+            selected_value = self.show_words.get('select.first', 'select.last')
+            len_selected_value = len(selected_value)
 
-        if get_value == selected_value:  # When user typed word and selected word are same
-            self.correct_words += 1
-            self.select_word('#008000', 'correct')
-            self.correct_keystrokes += len_selected_value
+            self.TotalKeyWords += len_selected_value
 
-        elif get_value != selected_value:  # When user typed word and selected word are not same
-            self.incorrect_words += 1
-            self.select_word('red', 'incorrect')
-            self.incorrect_keystrokes += len_selected_value
+            if get_value == selected_value:  # When user typed word and selected word are same
+                self.CorrectWords += 1
+                self.select_word('#008000', 'correct')
+                self.CorrectKeyStrokes += len_selected_value
 
-        self.prev_index = f'{self.space_index[0]}+1c'
-        self.space_index.pop(0)
-        self.select_word()
+            else:  # When user typed word and selected word are not same
+                self.IncorrectWords += 1
+                self.select_word('red', 'incorrect')
+                self.IncorrectKeyStrokes += len_selected_value
 
-        self.typing_area_var.set('')
-        self.show_words.see(self.space_index[0])
+            self.prev_index = f'{self.space_index[0]}+1c'
+            self.space_index.pop(0)
+            self.select_word()
+
+            self.typing_area_var.set('')
+            self.show_words.see(self.space_index[0])
 
         return 'break'
 
@@ -223,25 +255,23 @@ class Test_Typing_Speed:
             if self.timer:
                 self.master.after_cancel(self.timer)
 
-        elif self.time > 0:  # Decrease time by 1 second and call this function again and again until time reaches to 0
-            self.time -= 1
-            self.time_label_var.set(f'00:{str(self.time).zfill(2)}')
+        elif self.TotalTime > 0:  # Decrease time by 1 second and call this function again and again until time reaches to 0
+            self.TotalTime -= 1
+            self.time_label_var.set(f'00:{str(self.TotalTime).zfill(2)}')
             self.timer = self.master.after(1000, self.track_time)
 
         else:  # When time reaches to 0
             self.show_words.pack_forget()
 
             try:  # Calculating WPM
-                gross_wpm = int(round(self.correct_keystrokes / 5, 0))
-                accuracy = round((self.correct_keystrokes - self.incorrect_keystrokes - self.backspace_counter) / self.total_keywords * 100, 2)
+                gross_wpm = int(round(self.CorrectKeyStrokes / 5, 0))
+                accuracy = round((self.CorrectKeyStrokes / (self.TotalKeyWords + self.BackSpaceCounter)) * 100, 2)
 
                 if accuracy < 0:
                     accuracy = 0
 
             except ZeroDivisionError:
                 gross_wpm = accuracy = 0
-
-            self.typing_area_var.set('')
 
             self.output_frame = Frame(self.master)
 
@@ -250,16 +280,16 @@ class Test_Typing_Speed:
             wpm_label = Label(wpm_frame, text=f'{gross_wpm} WPM', fg='green', font=font.Font(size=20, weight='bold'))
             wpm_label.grid(row=0, column=0, sticky='w')
 
-            full_from_label = Label(wpm_frame, text='(word per minute)', font=font.Font(size=13, weight='bold'))
+            full_from_label = Label(wpm_frame, text='(words per minute)', font=font.Font(size=13, weight='bold'), takefocus=False)
             full_from_label.grid(row=0, column=1, sticky='w')
             wpm_frame.grid(row=0, column=0, sticky='w')
 
-            text_1 = f'{"Keystrokes".ljust(20)}({self.correct_keystrokes} | {self.incorrect_keystrokes}) {self.total_keywords}\n'
+            text_1 = f'{"Keystrokes".ljust(20)}({self.CorrectKeyStrokes} | {self.IncorrectKeyStrokes}) {self.TotalKeyWords}\n'
             text_2 = f'{"Accuracy".ljust(20)}{accuracy} %\n'
-            text_3 = f'{"Correct Words".ljust(20)}{self.correct_words}\n'
-            text_4 = f'{"Wrong Words".ljust(20)}{self.incorrect_words}'
+            text_3 = f'{"Correct Words".ljust(20)}{self.CorrectWords}\n'
+            text_4 = f'{"Wrong Words".ljust(20)}{self.IncorrectWords}'
 
-            text_widget = Text(self.output_frame, height=5, width=50, bg='#f0f0f0', border=0, cursor='arrow')
+            text_widget = Text(self.output_frame, height=5, width=50, bg='#f0f0f0', border=0, cursor='arrow', takefocus=False)
             text_widget.insert('1.0', f'{text_1}{text_2}{text_3}{text_4}')
 
             pip_index = text_widget.search("|", "1.21", "1.end")
@@ -267,10 +297,10 @@ class Test_Typing_Speed:
             text_widget.tag_config('correct', foreground='green', font=font.Font(weight='bold', size=9))
             text_widget.tag_add('incorrect', f"{pip_index}+2c", text_widget.search(')', pip_index, '1.end'))
             text_widget.tag_config('incorrect', foreground='red', font=font.Font(weight='bold', size=9))
-            text_widget.tag_add('corrcet_word', '3.20', '3.end')
-            text_widget.tag_config('corrcet_word', foreground='green', font=font.Font(weight='bold'))
-            text_widget.tag_add('incorrcet_word', '4.20', '4.end')
-            text_widget.tag_config('incorrcet_word', foreground='red', font=font.Font(weight='bold'))
+            text_widget.tag_add('correct_word', '3.20', '3.end')
+            text_widget.tag_config('correct_word', foreground='green', font=font.Font(weight='bold'))
+            text_widget.tag_add('incorrect_word', '4.20', '4.end')
+            text_widget.tag_config('incorrect_word', foreground='red', font=font.Font(weight='bold'))
 
             text_widget.config(state='disabled')
             text_widget.grid(row=1, column=0)
@@ -279,23 +309,13 @@ class Test_Typing_Speed:
             text_widget.bind('<Leave>', self.forbid_default_bindings)
             text_widget.bind('<Motion>', self.forbid_default_bindings)
             text_widget.bind('<Double-Button-1>', self.forbid_default_bindings)
+
             self.output_frame.pack()
 
     def redo(self, event=None):
         '''Reset everything to the beginning'''
 
-        self.time = 60
-        self.running = False
-        self.prev_index = '1.0'
-        self.typing_area_var.set('')
-        self.time_label_var.set('00:00')
-
-        self.total_keywords = 0
-        self.correct_words = 0
-        self.incorrect_words = 0
-        self.backspace_counter = 0
-        self.correct_keystrokes = 0
-        self.incorrect_keystrokes = 0
+        self.default_values()
 
         try:
             self.output_frame.destroy()
