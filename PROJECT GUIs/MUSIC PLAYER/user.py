ab1e5f3cf881e779e73df4d61e1231f4a9be7170
ghanteyyy <ghanteyyy@gmail.com>
2022-03-28 08:06:14 +0545

    UI themes

---
+++ PROJECT GUIs/MUSIC PLAYER/user.py
@@ -0,0 +1,23 @@
+USER_THEMES = {
+    "newtheme": {
+        "type": "dark",
+        "colors": {
+            "primary": "#babebc",
+            "secondary": "#2e2e2e",
+            "success": "#3498db",
+            "info": "#3498db",
+            "warning": "#f39c12",
+            "danger": "#e74c3c",
+            "light": "#f0ad4e",
+            "dark": "#343A40",
+            "bg": "#babebc",
+            "fg": "#000000",
+            "selectbg": "#4b4b4b",
+            "selectfg": "#ffffff",
+            "border": "#222222",
+            "inputfg": "#333333",
+            "inputbg": "#e5e5e5",
+            "active": "#e5e5e5"
+        }
+    }
+}
