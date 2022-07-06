dede7d5b6dcc426accc0260f6e4ecfc420fd760f
ghanteyyy <ghanteyyy@gmail.com>
2022-07-06 20:47:03 +0545

    Added functionality to focus out of Entry widget when TAB key is pressed Focus remains to the Entry widget after user clicks to OK button in message-box Added functionality to delete the last first whole word when pressed ctrl+backspace Showing white image then the actual QR image when user presses ctrl+c to make like an animation

--- PROJECT GUIs/QRCODE GENERATOR/qrcode_generator.py
+++ PROJECT GUIs/QRCODE GENERATOR/qrcode_generator.py
@@ -8,6 +8,7 @@ from tkinter.font import Font
 from tkinter import filedialog
 from tkinter import messagebox
 import qrcode
+import pywintypes
 import win32clipboard
 from PIL import Image, ImageTk
 
@@ -46,12 +47,14 @@ class QRCODE_GENERATOR:
         self.make_QR(self.QR_DEFAULT_TEXT)
         self.InitialPosition()
 
+        self.master.bind('<Tab>', self.tab_focus_out)
         self.master.bind('<Button-1>', self.focus_here)
         self.message_box.bind('<FocusIn>', self.focus_in)
         self.qr_label.bind('<Button-3>', self.RightClick)
         self.message_box.bind('<FocusOut>', self.focus_out)
         self.master.bind_all('<Control-s>', self.SaveImage)
         self.master.bind_all('<Control-c>', self.CopyImage)
+        self.message_box.bind('<Control-BackSpace>', self.ControlBackSpace)
 
         self.master.mainloop()
 
@@ -68,6 +71,8 @@ class QRCODE_GENERATOR:
 
         self.master.geometry(f'+{screen_width - width}+{screen_height - height}')
         self.master.deiconify()
+
+        self.master.update()
 
     def focus_here(self, event=None):
         '''Focus to the clicked widget'''
@@ -91,6 +96,26 @@ class QRCODE_GENERATOR:
             self.message_box_var.set(self.DEFAULT_TEXT)
             self.message_box_style.configure('MB.TEntry', foreground='grey')
 
+    def tab_focus_out(self, event=None):
+        '''Remove focus from Entry widget when already
+           focused and still user presses TAB key'''
+
+        if event.widget == self.message_box:
+            self.master.focus()
+            return 'break'
+
+    def ControlBackSpace(self, event=None):
+        '''Delete a word before a white-space'''
+
+        var_get = self.message_box_var.get()
+        word_splits = var_get.split()
+        word_splits = word_splits[:len(word_splits) - 1]
+
+        word_splits_join = ' '.join(word_splits)
+        self.message_box_var.set(word_splits_join)
+
+        return 'break'
+
     def trace_var(self, *args):
         '''When user changes value in entry widget'''
 
@@ -105,21 +130,19 @@ class QRCODE_GENERATOR:
         '''Make QR code based on the text inserted in the entry widget'''
 
         try:
-            img = self.img = qrcode.make(data)
+            self.img = self.QRImage = qrcode.make(data)
 
-            if self.img.size[1] > 290:  # Resize generated QR-code image when its height exceeds 290
-                img = self.img.resize((290, 290), Image.Resampling.LANCZOS)
+            if self.QRImage.size[1] > 290:  # Resize generated QR-code image when its height exceeds 290
+                self.img = self.QRImage.resize((290, 290), Image.Resampling.LANCZOS)
 
-            img = ImageTk.PhotoImage(image=img)
+            self.img = ImageTk.PhotoImage(image=self.img)
 
-            self.qr_label.config(image=img)
-            self.qr_label.image = img
+            self.qr_label.config(image=self.img)
+            self.qr_label.image = self.img
 
         except qrcode.exceptions.DataOverflowError:
             messagebox.showinfo('ERR', 'Upto 2,394 characters are allowed')
-
             self.message_box_var.set('')
-            self.master.focus()
 
     def SaveImage(self, event=None):
         '''Save QR code to the user selected location'''
@@ -128,21 +151,29 @@ class QRCODE_GENERATOR:
         path = filedialog.asksaveasfilename(title='Save', filetypes=file_types, defaultextension=([file_types[0]]))
 
         if path:  # Save image only when user have given valid location
-            self.img.save(path)
+            self.QRImage.save(path)
 
     def CopyImage(self, event=None):
         '''Copy image to clipboard'''
 
         memory = io.BytesIO()
-        self.img.convert('RGB').save(memory, format='BMP')
+        self.QRImage.convert('RGB').save(memory, format='BMP')
 
         if sys.platform == 'win32':
             data = memory.getvalue()[14:]
 
-            win32clipboard.OpenClipboard()
-            win32clipboard.EmptyClipboard()
-            win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
-            win32clipboard.CloseClipboard()
+            try:
+                win32clipboard.OpenClipboard()
+                win32clipboard.EmptyClipboard()
+                win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+                win32clipboard.CloseClipboard()
+
+            except pywintypes.error:
+                # Encountered pywintypes.error when pressed
+                # control-c continuously to copy image in clipboard.
+                # There should be some interval while opening
+                # clipboard frequently
+                pass
 
         else:
             # I do not have a linux machine so I have no idea if the
@@ -150,6 +181,20 @@ class QRCODE_GENERATOR:
             output = subprocess.Popen(("xclip", "-selection", "clipboard", "-t", "image/png", "-i"), stdin=subprocess.PIPE)
             output.stdin.write(memory.getvalue())  # write image to stdin
             output.stdin.close()
+
+        # Replacing qr image with blank white image
+        # to inform user that image has been captured
+        img = PhotoImage(file=self.ResourcePath('blank.png'))
+        self.qr_label.config(image=img)
+        self.qr_label.image = img
+
+        self.master.after(200, self.restore_QR_Image)
+
+    def restore_QR_Image(self):
+        '''Replace blank white image with QR image'''
+
+        self.qr_label.config(image=self.img)
+        self.qr_label.image = self.img
 
     def RightClick(self, event=None):
         '''When user right clicks to QR image'''
