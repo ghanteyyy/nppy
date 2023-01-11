8f7c2368a656236274e6af780d205b6bc15db15c
ghanteyyy <ghanteyyy@gmail.com>
2023-01-11 21:16:29 +0545

    Updated UI Replaced txt file with json to store data Refactored code

--- PROJECT GUIs/BIRTHDAY REMAINDER/BirthdayRemainder.py
+++ PROJECT GUIs/BIRTHDAY REMAINDER/BirthdayRemainder.py
@@ -1,103 +1,174 @@
 import os
 import sys
+import uuid
+import json
+import calendar
 from tkinter import *
 import tkinter.ttk as ttk
+from tkinter import filedialog
 import tkinter.messagebox as messagebox
+from PIL import Image
 
 
-class BirthdayRemainder:
+class BirthdayReminder:
     def __init__(self):
-        self.file = os.path.abspath(os.path.join('.', 'birthday_remainder.txt'))
-        self.month_number = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
-        self.label_attributes = {'fg': 'white', 'font': ('Courier', 12), 'bg': 'dark green'}
+        self.ImagePath = ''
+        self.NameEntryDefaultText = True
+        self.MonthNames = list(calendar.month_abbr)[1:]
+        self.ImageDirPath = self.ResourcePath('images')
+        self.FILE = os.path.abspath(os.path.join('.', 'data.json'))
 
         self.master = Tk()
         self.master.withdraw()
-        self.master.resizable(0, 0)
-        self.master.iconbitmap(self.resource_path('icon.ico'))
         self.master.title('Birthday Remainder')
 
-        self.width, self.height = 426, 470
-        self.screen_width, self.screen_height = self.master.winfo_screenwidth(), self.master.winfo_screenheight()
-        self.master.geometry(f'{self.width}x{self.height}+{self.screen_width // 2 - self.width // 2}+{self.screen_height // 2 - self.height // 2}')
+        self.BirthdayQuoteImage = Image.open(self.ResourcePath('image.png'))
+        width, height = self.BirthdayQuoteImage.size
+        self.BirthdayQuoteImage = PhotoImage(file=self.ResourcePath('image.png'))
 
-        self.birthday_quote_frame = Frame(self.master)
-        self.birthday_quote_image = PhotoImage(file=self.resource_path('image.png'))
-        self.label_birthday_quote = Label(self.birthday_quote_frame, image=self.birthday_quote_image)
-        self.label_birthday_quote.grid(row=0, column=0)
-        self.birthday_quote_frame.place(x=0, y=0)
+        self.BirthdayQuoteLabel = Label(self.master, image=self.BirthdayQuoteImage, width=width-10, height=height-2)
+        self.BirthdayQuoteLabel.pack(padx=0, pady=0)
 
-        self.label_entry_frame = Frame(self.master, bg='dark green')
-        self.name_label = Label(self.label_entry_frame, text='Name', **self.label_attributes)
-        self.name_box = Entry(self.label_entry_frame, width=30)
-        self.name_label.grid(row=0, column=0)
-        self.name_box.grid(row=0, column=1, padx=30, pady=20, ipady=2)
+        self.NameEntryVar = StringVar()
+        self.NameEntryVar.set('Name')
+        self.NameEntry = Entry(self.master, width=35, textvariable=self.NameEntryVar, foreground='grey', justify='center')
+        self.NameEntry.pack(ipady=3, pady=20)
 
-        self.birthday_label = Label(self.label_entry_frame, text='Date of Birth', **self.label_attributes)
-        self.birthday_label.grid(row=1, column=0)
-        self.label_entry_frame.place(x=70, y=240)
+        self.ComboFrame = LabelFrame(self.master, bg='dark green', text='Date of Birth', fg='white')
+        self.ComboFrame.pack(pady=(0, 20))
+        self.MonthCombo = ttk.Combobox(self.ComboFrame, values=self.MonthNames, width=12, height=12)
+        self.MonthCombo.set('Select Month')
+        self.MonthCombo.grid(row=0, column=0, padx=15, pady=15)
+        self.DateCombo = ttk.Combobox(self.ComboFrame, values=list(range(1, 32)), width=10, height=12)
+        self.DateCombo.set('Select Date')
+        self.DateCombo.grid(row=0, column=1, padx=(0, 15))
 
-        self.combo_box_frame = Frame(self.master, bg='dark green')
-        self.month_box = ttk.Combobox(self.combo_box_frame, values=[month for month in self.month_number], width=12, height=9)
-        self.month_box.set('Select Month')
-        self.month_box.grid(row=0, column=0)
+        self.ChooseImageButton = Button(self.master, text='Choose Image', fg='white', activeforeground='white', bg='#004503', activebackground='#004503', bd=0, width=30, cursor='hand2', command=self.ChooseImageButtonCommand)
+        self.ChooseImageButton.pack(pady=(0, 20), ipady=10)
 
-        self.date_box = ttk.Combobox(self.combo_box_frame, values=[i for i in range(1, 32)], width=10, height=9)
-        self.date_box.set('Select Date')
-        self.date_box.grid(row=0, column=1, padx=5)
-
-        self.combo_box_frame.place(x=235, y=305)
-
-        self.var = IntVar()
+        self.RadioVar = IntVar()
         self.style = ttk.Style()
         self.style.configure('R.TRadiobutton', background='dark green', foreground='white')
 
-        self.radio_frame = Frame(self.master, bg='dark green')
-        self.add_radio_button = ttk.Radiobutton(self.radio_frame, text='Add', value=1, variable=self.var, style='R.TRadiobutton', cursor='hand2')
-        self.delete_radio_button = ttk.Radiobutton(self.radio_frame, text='Delete', value=2, variable=self.var, style='R.TRadiobutton', cursor='hand2')
-        self.add_radio_button.grid(row=0, column=0, padx=10)
-        self.delete_radio_button.grid(row=0, column=1)
-        self.radio_frame.place(x=255, y=350)
+        self.RadioFrame = Frame(self.master, bg='dark green')
+        self.AddRadioButton = ttk.Radiobutton(self.RadioFrame, text='Add', value=1, variable=self.RadioVar, style='R.TRadiobutton', cursor='hand2')
+        self.DeleteRadioButton = ttk.Radiobutton(self.RadioFrame, text='Delete', value=2, variable=self.RadioVar, style='R.TRadiobutton', cursor='hand2')
+        self.AddRadioButton.grid(row=0, column=0, padx=10)
+        self.DeleteRadioButton.grid(row=0, column=1)
+        self.RadioFrame.pack(pady=(0, 20))
 
-        self.button_frame = Frame(self.master)
-        self.submit_button = Button(self.button_frame, text='SUBMIT', fg='white', bg='#039e05', activebackground='#039e05', activeforeground='white', cursor='hand2', font=('Courier', 12), width=16, height=3, bd=0, relief=GROOVE, command=self.submit_command)
-        self.submit_button.grid(row=2, column=0)
-        self.button_frame.place(x=250, y=390)
+        self.SubmitButton = Button(self.master, text='SUBMIT', fg='white', bg='#039e05', activebackground='#039e05', activeforeground='white', cursor='hand2', font=('Courier', 12), width=16, height=3, bd=0, relief=GROOVE, command=self.SubmitCommand)
+        self.SubmitButton.pack(pady=(0, 20))
 
-        self.name_box.bind('<Return>', self.submit_command)
-        self.date_box.bind('<Return>', self.submit_command)
-        self.master.bind('<Button-1>', self.master_bindings)
+        self.DateCombo.bind('<Return>', self.SubmitCommand)
+        self.NameEntry.bind('<Return>', self.SubmitCommand)
+        self.master.bind('<Button-1>', self.MasterBindings)
+        self.NameEntry.bind('<FocusIn>', self.EntryFocusIn)
+        self.NameEntry.bind('<FocusOut>', self.EntryFocusOut)
+        self.NameEntry.bind('<Shift-BackSpace>', self.BulkDelete)
 
-        self.master.after(0, self.master.deiconify)
+        self.InitialPosition()
         self.master.config(bg='dark green')
+
         self.master.mainloop()
 
-    def master_bindings(self, event=None):
-        '''When user clicks outside of the Entry box'''
+    def InitialPosition(self):
+        '''
+        Set GUI window to the center of
+        the screen when it starts
+        '''
 
-        if event.widget not in [self.name_box, self.month_box, self.date_box]:
-            if not self.month_box.get().strip():
-                self.month_box.set('Select Month')
+        self.master.update()
 
-            if not self.date_box.get().strip():
-                self.date_box.set('Select Date')
+        width = self.master.winfo_width()// 2
+        height = self.master.winfo_height() // 2
+        screen_width = self.master.winfo_screenwidth() // 2
+        screen_height = self.master.winfo_screenheight() // 2
+
+        self.master.resizable(0, 0)
+        icon_photo = PhotoImage(file=self.ResourcePath('icon.png'))
+        self.master.iconphoto(False, icon_photo)
+
+        self.master.geometry(f'+{screen_width - width}+{screen_height - height}')
+        self.master.after(0, self.master.deiconify)
+
+    def EntryFocusIn(self, event=None):
+        '''
+        Remove the default text when user
+        clicks to the entry widget
+        '''
+
+        if self.NameEntryDefaultText:
+            self.NameEntryVar.set('')
+            self.NameEntryDefaultText = False
+            self.NameEntry.config(foreground='black')
+
+    def EntryFocusOut(self, event=None):
+        '''
+        Re-insert the default text to entry widget
+        when user clicks outside of the entry widget
+        without entering any value in it
+        '''
+
+        if not self.NameEntryVar.get().strip():
+            self.NameEntryVar.set('Name')
+            self.NameEntryDefaultText = True
+            self.NameEntry.config(foreground='grey')
+
+    def MasterBindings(self, event=None):
+        '''
+        When user clicks outside of the Entry box
+        '''
+
+        if event.widget not in [self.NameEntry, self.MonthCombo, self.DateCombo]:
+            if not self.MonthCombo.get().strip():
+                self.MonthCombo.set('Select Month')
+
+            if not self.DateCombo.get().strip():
+                self.DateCombo.set('Select Date')
+
+            if not self.NameEntryVar.get().strip():
+                self.NameEntryDefaultText = False
 
             self.master.focus()
 
-    def submit_command(self, event=None):
-        '''Command for submit button'''
+    def BulkDelete(self, event=None):
+        '''
+        Delete last word of the value in entry widget
+        '''
 
-        success = False
-        var = self.var.get()
-        day = self.date_box.get().strip()
-        month = self.month_box.get().strip()
-        name = self.name_box.get().strip().upper()
+        if self.NameEntryDefaultText is False:
+            from_entry = self.NameEntryVar.get().split()
+            from_entry = ' '.join(from_entry[:-1])
+            self.NameEntryVar.set(from_entry)
 
-        if not name or not month:
-            messagebox.showerror('No Input', 'Name and date of birthday was expected')
-            self.name_box.delete(0, END)
+            return 'break'
 
-        elif month not in self.month_number:
+    def ChooseImageButtonCommand(self):
+        '''
+        Command for ChooseImageButton to select an image
+        '''
+
+        extensions = ([('All', '*png *jpg *jpeg'), ('PNG', '*png'), ('JPG', '*jpg *jpeg')])
+        self.ImagePath = filedialog.askopenfilename(title='Select an Image', defaultextension=extensions, filetypes=extensions)
+
+        if self.ImagePath:
+            self.ChooseImageButton.config(text=os.path.basename(self.ImagePath))
+
+    def SubmitCommand(self, event=None):
+        '''
+        Command for submit button
+        '''
+
+        var = self.RadioVar.get()
+        day = self.DateCombo.get().strip()
+        month = self.MonthCombo.get().strip()
+        name = self.NameEntry.get().strip().upper()
+
+        if self.NameEntryDefaultText:
+            messagebox.showerror('No Input', 'Please enter the valid name')
+
+        elif month not in self.MonthNames:
             messagebox.showerror('Invalid Month', 'Month was expected between Jan-Dec')
 
         elif not day.isdigit() or not 0 < int(day) <= 32:
@@ -106,68 +177,94 @@ class BirthdayRemainder:
         elif var not in [1, 2]:
             messagebox.showerror('Invalid Option', 'Either Add or Remove button was expected')
 
+        elif not self.ImagePath.strip():
+            messagebox.showerror('Invalid Image', 'Please select a valid image')
+
         else:
-            if not os.path.exists(self.file):   # Creating file if not exists
-                with open(self.file, 'w'):
-                    pass
+            day = day.zfill(2)
+            id = uuid.uuid4().hex
+            month = str(self.MonthNames.index(month) + 1).zfill(2)
 
-            date = f'{self.month_number[month].zfill(2)}-{day.zfill(2)}'
-            details = f'{name.ljust(30)}:{date.rjust(10)}\n'
+            # Create assets/images directory if not exists
+            if os.path.exists(self.ImageDirPath) is False:
+                os.mkdir(self.ImageDirPath)
 
-            with open(self.file, 'r+') as f:
-                contents = f.readlines()
+            # Generating new_path with respect to assets/images directory for the selected image
+            random_image_name = f"{id}.{self.ImagePath.split('.')[-1]}"
+            new_path = os.path.join(self.ImageDirPath, random_image_name)
 
-                if var == 1:
-                    if details in contents:
-                        messagebox.showerror('EXISTS', 'Name and Date of Birth already exists in file')
+            # Copying image to assets/images
+            with open(self.ImagePath, "rb") as rb, open(new_path, 'wb') as wb:
+                contents = rb.read()
+                wb.write(contents)
 
-                    else:
-                        success = True
-                        contents.append(details)
-                        messagebox.showinfo('ADDED', 'Name and Date of Birth is added to the file')
+            # Saving details
+            contents = {
+                id:{
+                    'name': name,
+                    'date': f'{month}-{day}',
+                    'has_seen': False,
+                    'image': new_path
+                }
+            }
 
-                elif var == 2:
-                    if details not in contents:
-                        messagebox.showerror('Invalid', 'Name and Date of Birth is not in file')
+            self.WriteJSON(contents)
 
-                    else:
-                        success = True
-                        contents.remove(details)
-                        messagebox.showinfo('REMOVED', 'Name and Date of Birth removed from the file')
+            self.RadioVar.set(0)
+            self.NameEntryVar.set('')
+            self.EntryFocusOut()
+            self.DateCombo.set('Select Date')
+            self.MonthCombo.set('Select Month')
+            self.ChooseImageButton.config(text='Choose Image')
 
-                contents.sort()
-                f.seek(0)
+    def ReadJSON(self):
+        '''
+        Reading data from the .json file
+        '''
 
-                for content in contents:
-                    f.write(content)
+        try:
+            with open(self.FILE, 'r') as f:
+                contents = json.load(f)
 
-                f.truncate()
+        except FileNotFoundError:
+            with open(self.FILE, 'w'):
+                contents = {}
 
-            if success:
-                for widget in [self.date_box, self.name_box, self.month_box]:
-                    widget.delete(0, END)
+        except json.decoder.JSONDecodeError:
+            contents = {}
 
-                self.var.set(0)
-                self.date_box.set('Select Date')
-                self.month_box.set('Select Month')
+        return contents
 
-    def resource_path(self, file_name):
-        '''Get absolute path to resource from temporary directory
+    def WriteJSON(self, contents):
+        '''
+        Storing data to the .json file
+        '''
+
+        from_file = self.ReadJSON()
+        from_file.update(contents)
+
+        with open(self.FILE, 'w') as f:
+            json.dump(from_file, f, indent=4)
+
+    def ResourcePath(self, FileName):
+        '''
+        Get absolute path to resource from temporary directory
 
         In development:
             Gets path of files that are used in this script like icons, images or file of any extension from current directory
 
         After compiling to .exe with pyinstaller and using --add-data flag:
-            Gets path of files that are used in this script like icons, images or file of any extension from temporary directory'''
+            Gets path of files that are used in this script like icons, images or file of any extension from temporary directory
+        '''
 
         try:
-            base_path = sys._MEIPASS  # PyInstaller creates a temporary directory and stores path of that directory in _MEIPASS
+            BasePath = sys._MEIPASS  # PyInstaller creates a temporary directory and stores path of that directory in _MEIPASS
 
         except AttributeError:
-            base_path = os.path.dirname(__file__)
+            BasePath = os.path.dirname(__file__)
 
-        return os.path.join(base_path, 'assets', file_name)
+        return os.path.join(BasePath, 'assets', FileName)
 
 
 if __name__ == '__main__':
-    BirthdayRemainder()
+    BirthdayReminder()
